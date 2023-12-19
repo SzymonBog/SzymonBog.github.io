74f5bb2926a8d0a0786f914ddd4680eb962ea754
SzymonBog <92025573+SzymonBog@users.noreply.github.com>
2023-12-19 02:31:44 +0100

    Add files via upload

---
+++ websiteBase.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+<style>
+/*body {
+	background-image: url("background.jpg");
+	background-size: cover;
+	background-repeat: no-repeat;
+	background-color: navy;
+	color: white;
+	} */
+h1 {
+	color: gray;
+	margin-left: 40px;
+	}
+</style>
+
+/*<script src="podstrony/kolorujtlo.js" type="text/javascript"></script>*/
+
+<meta http-equiv="Content-type" content="text/html; charset=UTF-8" />
+<meta http-equiv="Content-Language" content="pl" />
+<meta name="Author" content="Szymon Bogdański" />
+<title>My website</title>
+</head>
+
+<body>
+
+    
+	
+</body>
+</html>
