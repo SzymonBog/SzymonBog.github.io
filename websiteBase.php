f5806c26b1b1e570afed4ad770647ba20383bc8c
SzymonBog <szymonbog77@gmail.com>
2024-01-19 00:41:34 +0100

    try

--- websiteBase.php
+++ websiteBase.php
@@ -26,7 +26,30 @@ h1 {
 
 <body>
 
-    
+<?php
+error_reporting(E_ALL ^ E_NOTICE ^ E_WARNING);
+/* po tym komentarzu będzie kod do dynamicznego ładowania stron */
+//$strona = "podstrony/glowna.html";
+//include($strona);
+if($_GET['idp']==''){
+	$website="subpages/main.html";
+} elseif($_GET['idp']=='1'){
+	$strona="podstrony/podstrona1.html";
+} elseif($_GET['idp']=='2'){
+	$strona="podstrony/podstrona2.html";
+} elseif($_GET['idp']=='3'){
+	$strona="podstrony/podstrona3.html";
+} elseif($_GET['idp']=='4'){
+	$strona="podstrony/podstrona4.html";
+} elseif($_GET['idp']=='5'){
+	$strona="podstrony/podstrona5.html";
+} elseif($_GET['idp']=='6'){
+	$strona="podstrony/kontakt.html";
+} else {
+	$strona="subpages/main.html";
+}
+include($website);
+?>
 	
 </body>
 </html>
